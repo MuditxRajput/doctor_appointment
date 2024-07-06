e39340c7b81e380bba1db9a0384219fd945d2ed7
Mudit Rajput <muditrajputpersonal@gmail.com>
2024-07-07 02:06:28 +0530

    'day2'

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import AboutSection from "@/components/AboutSection/page";
 import Hero from "@/components/Hero/page";
 import { databaseConnection } from "@/database/database";
-
+import Emergency from '../components/Emergency/page';
 const Homepage = () => {
   databaseConnection();
   return (
     <div>
       <Hero />
       <AboutSection/>
+      <Emergency/>
     </div>
   );
 };
